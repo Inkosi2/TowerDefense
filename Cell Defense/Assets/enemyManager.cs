68c81827a974739feec11b1c96debbe86551ffea
Unknown <oscarmasferrerrubio@enti.cat>
2017-10-13 16:15:13 +0200

    Enemies change

--- Cell Defense/Assets/enemyManager.cs
+++ Cell Defense/Assets/enemyManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
 
 public class enemyManager : MonoBehaviour {
 
@@ -7,6 +9,7 @@ public class enemyManager : MonoBehaviour {
     int aux;
     public GameObject virus;
     int i;
+    public Stack<GameObject> enemies = new Stack<GameObject>();
 
     void Start() {
         aux = 0;
@@ -16,7 +19,8 @@ public class enemyManager : MonoBehaviour {
     void Update() {
         if (aux < 5 && Input.GetKey("q"))
         {
-            Instantiate(virus, new Vector3(3.25f, -1.5f, 0.75f), Quaternion.Euler(0, 0, 0));
+            enemies.Push((GameObject)Instantiate(virus, new Vector3(3.25f, -1.5f, 0.75f), Quaternion.Euler(0, 0, 0)));
+            enemies.Peek().GetComponent<Virus>().i = 0;
             aux++;
         }
     }
